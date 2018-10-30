06ef00ea827bc05135374df66c038d04559bba74
Yegor Stephanovich <stefanovich_egor@mail.ru>
2018-10-30 11:16:02 +0300

    Implemented type definition classes

--- AssemblyLibrary/FieldDefinition.cs
+++ AssemblyLibrary/FieldDefinition.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,30 @@ namespace AssemblyLibrary
 {
      public class FieldDefinition
      {
+          public string fieldName;
+          public string fieldDefinition;
 
+          public FieldDefinition(FieldInfo fieldInfo)
+          {
+               this.fieldName = fieldInfo.Name;
+               this.fieldDefinition = TypeModifier.GetTypeDefinition(fieldInfo.GetType());
+
+               fieldDefinition += (fieldInfo.FieldType.IsGenericType ?
+                    String.Format("{0}<{1}>", fieldInfo.FieldType.Name, GetGenericType(fieldInfo.FieldType.GenericTypeArguments)) :
+                    fieldInfo.FieldType.Name);
+          }
+
+          private string GetGenericType(Type[] type)
+          {
+               string result = "";
+               foreach (Type genericType in type)
+               {
+                    result += (type.GetType().IsGenericType ?
+                         String.Format("<{0}>", GetGenericType(genericType.GetType().GenericTypeArguments)) :
+                         String.Format("<{0}> ", genericType.Name));
+               }
+
+               return result;
+          }
      }
 }
--- AssemblyLibrary/MethodDefinition.cs
+++ AssemblyLibrary/MethodDefinition.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,11 @@ namespace AssemblyLibrary
 {
      public class MethodDefinition
      {
+          public string methodDefinition;
 
+          public MethodDefinition(MethodInfo methodInfo)
+          {
+               methodDefinition = TypeModifier.GetTypeDefinition(methodInfo.GetType()) + methodInfo.ToString();
+          }
      }
 }
